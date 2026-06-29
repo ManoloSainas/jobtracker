f92210506c91041d18d50744a8ca97f1bf7d1278
Manolo Sainas <sainas.manolo@gmail.com>
2026-06-29 11:52:36 +0200

    Creati i tre Repository

--- src/main/java/com/manolo/jobtracker/repository/JobApplicationRepository.java
+++ src/main/java/com/manolo/jobtracker/repository/JobApplicationRepository.java
@@ -1,11 +1,18 @@
 package com.manolo.jobtracker.repository;
 
 import com.manolo.jobtracker.model.JobApplication;
+import com.manolo.jobtracker.model.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
 
     List<JobApplication> findByUserId(Long userId);
+
+    List<JobApplication> findByUserIdAndStatus(Long userId, Status status);
+
+    List<JobApplication> findByUserIdOrderByApplicationDateDesc(Long userId);
 }
